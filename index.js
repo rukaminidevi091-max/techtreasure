87687ca84aa3b5978e6c3f39a8e0520c17e84857
Amit kumar <rukaminidevi091@gmail.com>
2026-06-05 06:00:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,3 +10,51 @@ function toggle(el){
     icon.textContent = "−";
   }
 }
+
+const searchInput = document.getElementById("toolSearch");
+const searchBtn = document.getElementById("searchBtn");
+
+function searchFeatures() {
+
+    const searchText =
+    searchInput.value.toLowerCase().trim();
+
+    const features =
+    document.querySelectorAll(".feature");
+
+    let found = false;
+
+    features.forEach(feature => {
+
+        const text =
+        feature.textContent.toLowerCase();
+
+        if (text.includes(searchText)) {
+
+            feature.style.display = "block";
+            found = true;
+
+        } else {
+
+            feature.style.display = "none";
+        }
+
+    });
+
+}
+
+searchBtn.addEventListener(
+    "click",
+    searchFeatures
+);
+
+searchInput.addEventListener(
+    "keyup",
+    e => {
+
+        if (e.key === "Enter") {
+            searchFeatures();
+        }
+
+    }
+);
